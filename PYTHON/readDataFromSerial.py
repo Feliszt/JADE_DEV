2dd1f9d70061505d44acb7d07214cad2c556bdcf
Félix <fcote@kth.se>
2020-12-07 19:58:08 +0100

    updated python

--- PYTHON/readDataFromSerial.py
+++ PYTHON/readDataFromSerial.py
@@ -1,10 +1,20 @@
 import serial
+import numpy as np
+import matplotlib.pyplot as plt
 
 # configure serial port
 ser = serial.Serial('COM7', 57600, timeout=1)
 
+#
+array_size = 1
+weight_array = []
+
 # loop
+loop_incr = 0
 while True:
+    #
+    loop_incr += 1
+
     # decode
     ser_bytes = ser.readline()
     decoded_bytes = ser_bytes[0:-2].decode("utf-8")
@@ -15,5 +25,22 @@ while True:
     # get measure as flot
     curr_weight = float(decoded_bytes)
 
-    # debug
-    print(curr_weight)
+    # sliding array
+    weight_array.append(curr_weight)
+    if(len(weight_array) > array_size):
+        weight_array = weight_array[1:]
+
+    # mean
+    weight_mean = np.mean(weight_array)
+    weight_dev = np.std(weight_array)
+
+    # plot
+    plt.clf()
+    plt.plot(weight_array, 'b')
+    plt.pause(0.05)
+
+
+    #
+    print("mean = [{}]\tdev = [{}]".format(weight_mean, weight_dev))
+
+#plt.show()
